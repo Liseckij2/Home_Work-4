1af805d80c567f1d197f2cc29086fdf64886231f
Andrew Liseczkiy <45668739+Liseckij2@users.noreply.github.com>
2021-09-27 00:30:31 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,66 +1,65 @@
-let arr = [];   
-function ThisFoo(argument) {
-    
-do {
+let calculation;
+let firstOperand;
+let secondOperand;
+let result;
+let history = [];
 
-  let  calculation;
-  let firstOperand;
-  let secondOperand;
-  let oneOperand;
-  let result;
-
-  if (calculation !== calculation) {
+function calculationFunction(params) {
     do {
-        calculation = +prompt('Send Number');
-     }  while (calculation !== calculation);
-  }
+    do {
+    calculation = +prompt('Chose operation ...\n 1 - sum\n 2 - dif\n 3 - div\n 4 - mult\n 5 - sin\n 6 - cos\n 7 - pow\n 0 - other, exit');
+} while (calculation !== calculation);
+} while(calculation >= 8);
+if (calculation >= 1 && calculation <= 4 ) {
+   firstOperand = +prompt('First operand');
+   secondOperand = +prompt ('Second operand');
+}
+ if(calculation >= 5) {
+    firstOperand = +prompt('Operand');
+}
+return calculation,firstOperand,secondOperand;
+};
 
-do {
-  calculation = +prompt('Chose operation ...\n 1 - sum\n 2 - dif\n 3 - div\n 4 - mult\n 5 - sin\n 6 - cos\n 7 - pow\n 0 - other, exit');
-} while (calculation < 7 && calculation <= 0);
-
-
-    if (calculation <= 4) {
-     firstOperand = +prompt('Send  first Operand');
-      secondOperand = +prompt('Send second Operand');
-    }
+function calculationNumbers() {
     
-   else if (5 <= calculation) {
-         oneOperand = +prompt('send operand');
-    }
+switch(calculation) {
 
-switch (calculation) {
-  case 1:
-    result = firstOperand + secondOperand;
-    break;
-case 2:
-    result = firstOperand / secondOperand;
-    break;
-case 3:
-    result = firstOperand - secondOperand;
-    break;
-case 4:
-    result = firstOperand * secondOperand;
-    break;
-case 5:
-    result = Math.sin(oneOperand);
-    break;
-case 6:
-    result = Math.cos(oneOperand);
-    break;
-case 7:
-    result = Math.pow(oneOperand, 2);
-    break;
+    case 1:
+        result = firstOperand + secondOperand;
+        break;
+    case 2:
+        result = firstOperand / secondOperand;
+        break;
+    case 3:
+        result = firstOperand - secondOperand;
+        break;
+    case 4:
+        result = firstOperand * secondOperand;
+        break;
+    case 5:
+        result = Math.sin(firstOperand);
+        break;
+    case 6:
+        result = Math.cos(firstOperand);
+        break;
+    case 7:
+        result = Math.pow(firstOperand, 2);
+        break;
+
+}
+return alert(result);
 }
 
-    alert(result);
+function addToStory() {
+    history.push(result).reverse;
+    console.log(history);
+}
 
-    arr.push(result);   
-    console.log(arr);
-
-} while (confirm('You want repeat?'));
-};
-ThisFoo()
+do {
+calculationFunction();
+calculationNumbers();
+addToStory();
+}while(confirm('repeat?'));
 
 // do {
 // let calculation;
